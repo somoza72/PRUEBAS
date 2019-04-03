e66b81f84d9211f9cb9395082a5ce92d3d0492c4
ESY <ESY@SOMOZA>
2019-04-03 22:59:02 +0200

    2º caso incorporamos precioventa

--- PrimeroPasos/src/PRECIO_IVA.java
+++ PrimeroPasos/src/PRECIO_IVA.java
@@ -3,12 +3,13 @@ public class PRECIO_IVA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		final double precio,iva,preciototal;
+		final double precio,iva,preciototal, precioventa;
 		precio=23;
 		iva=11.5;
 		preciototal=precio+ precio*iva/100;
+		precioventa= precio+ precio*20/100;
 		
-		System.out.println(" el precio del producto con iva es " + preciototal);
+		System.out.println(" el precio del producto con iva es " + preciototal + " y el precio de venta sera " + precioventa);
 		
 				
 		
